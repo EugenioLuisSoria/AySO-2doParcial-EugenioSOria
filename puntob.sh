dd8aa87bfeec7816a53e5232b9ff29a2a0f82110
Eugenio Soria <eugeniosoria@gmail.com>
2024-11-21 13:58:06 -0300

    finalizando...

--- puntob.sh
+++ puntob.sh
@@ -1,30 +1,23 @@
-
-161 ● Desde el Ansible Management Node con IP 196.168.56.8 actualizar todos los paquetes
-e instalar apache2 en el host Desarrollo con IP 196.168.56.9
-162 ● En la siguiente imagen se detalla la información.
-
-C:\os\vm\UTN-FRA_SO_Vagrant\VagrantFiles\vmAMN213\
+..\vmAMN213\
 vagrant up
 vagrant ssh
 ssh-keygen
+cat .ssh/id_rsa.pub
 
-C:\os\vm\UTN-FRA_SO_Vagrant\VagrantFiles\vmAnsDev213\
+..\vmAnsDev213\
 vagrant up
 vagrant ssh
 
-cat .ssh/id_rsa.pub
-
-echo "ssh-key" >> authorized_keys
-
+echo [ssh-key] >> authorized_keys
 
 git clone https://github.com/upszot/UTN-FRA_SO_Ansible.git
 
 UTN-FRA_SO_Ansible/ejemplo_02/inventory
    [desarrollo]
    192.168.56.9
-216
+
 UTN-FRA_SO_Ansible/ejemplo_02/playbook.yml
-modificar el playbook: elimino los últimos tres módulos:
+#[modificar el playbook: elimino los últimos tres módulos:]
 ---
 - hosts:
 - all
